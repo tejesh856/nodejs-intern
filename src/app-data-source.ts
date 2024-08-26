9bb0ea4d726e600e51f61ed9e6798bba91f128b4
tejesh reddy <tejeshvit007@gmail.com>
2024-08-26 22:15:13 +0530

    ready for production

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -10,7 +10,7 @@ export const myDataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: ["src/entity/*.js"],
+  entities: ["src/entity/*.ts"],
   logging: true,
   synchronize: true,
 });
